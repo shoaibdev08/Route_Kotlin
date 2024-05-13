641556ee8361c252a21e20286000f3e5984bdefe
Berlin <shoaib.ahmed08092@gmail.com>
2024-05-13 11:21:34 +0500

    Routes

---
+++ app/src/main/java/com/example/pas/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.pas
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+        val btn= findViewById<Button>(R.id.button2)
+        btn.setOnClickListener{
+            startActivity(Intent(this, shdata::class.java))
+        }
+    }
+}
